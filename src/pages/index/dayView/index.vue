ff150f108e3ed316a1771cb714f28d89d6283b54
RosyrRais <syr050301@qq.com>
2025-04-10 17:24:02 +0800

    chore: 重构项目目录 符合开发习惯 将所有行内样式修改至外部引入

---
+++ src/pages/index/dayView/index.vue
@@ -0,0 +1,63 @@
+<template>
+<view class="dayView">
+  <view class="title">
+    <text> 大会议程 </text>
+  </view>
+  <view class="search">
+    <nut-input v-model="searchInput" placeholder="输入会议名称进行搜索" />
+  </view>
+  <view class="meeting" v-for="meeting in meetingData" @tap="pageToDetail">
+    <view class="date">{{ meeting.data }}</view>
+    <view class="meeting-details">
+      <Image
+        :src="meeting.imgUrl"
+        mode="widthFix"
+        class="meeting-image"
+      />
+      <text class="meeting-title">{{ meeting.title }}</text>
+      <view class="meeting-time">
+        <Clock />
+        <text>{{ meeting.time }}</text>
+      </view>
+      <view class="meeting-location">
+        <location2 />
+        <text>{{ meeting.location }}</text>
+      </view>
+    </view>
+  </view>
+</view>
+</template>
+
+<script setup lang="ts">
+import { Location2, Clock } from '@nutui/icons-vue-taro'
+import { ref } from 'vue';
+import "./index.scss";
+import Taro from '@tarojs/taro';
+
+const searchInput = ref("");
+
+const pageToDetail = () => {
+  Taro.navigateTo({
+    url: "/pages/meetingDetail/index"
+  })
+}
+
+// mock
+const meetingData = [
+  {
+    data: "4月9日",
+    imgUrl: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
+    title: "教育系统会议",
+    time: "9：00-10：00",
+    location: "安恒大厦一楼"
+  },
+  {
+    data: "4月9日",
+    imgUrl: "https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg",
+    title: "教育系统会议",
+    time: "9：00-10：00",
+    location: "安恒大厦一楼"
+  },
+]
+
+</script>
--- src/pages/meetingDetail/index.vue
+++ src/pages/meetingDetail/index.vue
@@ -1,7 +1,9 @@
 <template>
-
+  <view>
+    大会议程
+  </view>
 </template>
 
 <script setup lang="ts">
-// import "./index.scss";
+
 </script>
