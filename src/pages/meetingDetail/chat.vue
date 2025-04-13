f8cc0342b924ae752713a285659f695696199351
RosyrRais <syr050301@qq.com>
2025-04-13 10:53:46 +0800

    feat(meetingDetail): 增加详情页

---
+++ src/pages/meetingDetail/chat.vue
@@ -0,0 +1,42 @@
+<template>
+  <view class="chat">
+    <view class="text-area">
+      <view class="words" v-for="word in chatData">
+        <view class="username">{{ word[0] }}</view>
+        <view class="content">{{ word[1] }}</view>
+      </view>
+    </view>
+    <view class="input-area">
+      <input type="text" class="input"/>
+      <view class="primary-btn">发送</view>
+    </view>
+  </view>
+</template>
+
+<script setup lang="ts">
+import "./chat.scss";
+
+// mock
+const chatData = [
+  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],
+  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],  ['user1', '这就是我想看的'],
+  ['user2', '这就是我想看的'],
+  ['user3', '这就是我想看的'],
+  ['user4', '这就是我想看的'],
+]
+</script>
